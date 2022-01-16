bfd8cab01cfa79ba959bf24946688f2631a4fce2
nicholas-28 <nika199828@mail.ru>
2022-01-17 01:35:48 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,41 +1,14 @@
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-// using getElementById
-document.getElementById("currentdate").textContent = fulldate;
-// ****************************
-// using querySelector
-const date2 = document.querySelector('#currentdate2');
+const date = new Date();
 
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  date2.textContent = new Date().toLocaleDateString('en-UK', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
+let currentYear = date.getFullYear();
+
+let homeYear = document.querySelector('#home-year');
+
+homeYear.innerHTML = currentYear;
+
+
+let lastModification = document.lastModified;
+
+let lastUpdated = document.getElementById('last-updated');
+lastUpdated.innerHTML = lastModification;
+console.log(lastModification)
