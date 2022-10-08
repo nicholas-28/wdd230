d4f3b6721094b137980d8fed06a8533d72387fda
nicholas-28 <nika199828@mail.ru>
2022-10-08 19:30:23 +0400

    home page 1st part

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+const datefield = document.querySelector(".date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
