064ef6241d58f5173fa98a76a8b991a4dfea2337
nicholas-28 <nika199828@mail.ru>
2022-10-11 23:55:56 +0400

    date script change

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -6,14 +6,18 @@ function toggleMenu() {
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
-const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector(".date");
 
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
 
-datefield.innerHTML = `<em>${fulldate}</em>`;
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
 
 const date = new Date();
 
