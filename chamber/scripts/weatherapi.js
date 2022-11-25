66621049f316888e9bbd7593538b8743f8ae03db
nicholas-28 <nika199828@mail.ru>
2022-11-25 17:53:49 +0400

    weatherapi home page

---
+++ chamber/scripts/weatherapi.js
@@ -0,0 +1,43 @@
+const currentTemp = document.querySelector('#temperature');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#condition');
+const windSpeed = document.querySelector('#windspeed')
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Tbilisi&units=imperial&appid=992d5080d5389496bd0a7225f376c28e';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+function toUpperCase(str) {
+    const arr = str.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    const str2 = arr.join(" ");
+    return str2
+}
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = toUpperCase(desc);
+    windSpeed.textContent =  weatherData.wind.speed;
+}
+
+apiFetch();
