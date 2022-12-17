75476f528c258876c05a12c71284bad275e6fb7b
nicholas-28 <nika199828@mail.ru>
2022-12-17 19:16:53 +0400

    fruits json checkbox

--- finalproject/js/checkbox.js
+++ finalproject/js/checkbox.js
@@ -1,4 +1,4 @@
-const requestURL = "";
+const requestURL = "https://nicholas-28.github.io/wdd230/finalproject/json/fruits.json";
 const checkbox = document.querySelector('.checkbox');
 
 fetch(requestURL)
@@ -7,52 +7,17 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         const fruits = jsonObject['fruits'];
+        console.log(fruits)
         fruits.forEach(displayFruits);
     });
 
-function displayCompanies(company) {
-    let card = document.createElement('section');
-    let image = document.createElement('img');
-    let name = document.createElement('h1');
-    let address = document.createElement('p');
-    let phone = document.createElement('p');
-    let web = document.createElement('a');
-    let membership = document.createElement('p');
+function displayFruits(fruit) {
+    const fruits = fruit.name;
+    const card = document.createElement('section');
+    card.innerHTML = `
+    <input type="checkbox" id="${fruits}" name="fruits" value="${fruits}">
+    <label for="${fruits}">${fruits}</label><br>`;
 
-    name.textContent = company.name;
-    address.textContent =`📍${company.address}`;
-    phone.textContent = `☎️ ${company.phone}`;
-    web.textContent = `🌐 ${company.name} site`;
-    membership.textContent = `👥 ${company.membership}`;
-
-    web.setAttribute('href', company.url);
-    image.setAttribute('src', company.image);
-    image.setAttribute('alt', `Logo of ${company.name}`);
-    image.setAttribute('loading', 'lazy');
-
-    card.appendChild(image);
-    card.appendChild(name);
-    card.appendChild(address);
-    card.appendChild(phone);
-    card.appendChild(web);
-    card.appendChild(membership);
-
-    document.querySelector('article.discovery').appendChild(card);
-}
-
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid-disc");
-	display.classList.remove("list-disc");
-});
-
-listbutton.addEventListener("click", showList); 
-
-function showList() {
-	display.classList.add("list-disc");
-	display.classList.remove("grid-disc");
+    document.querySelector('.checkbox').appendChild(card);
+    
 }
